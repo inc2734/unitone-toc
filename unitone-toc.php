952f6b4efb5b55a9e054c16c68c5f21b2a151683
inc2734 <inc@2inc.org>
2026-08-18 10:59:06 +0900

    1.1.3

--- unitone-toc.php
+++ unitone-toc.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin name: unitone TOC
- * Version: 1.1.2
- * Tested up to: 7.0
+ * Version: 1.1.3
+ * Tested up to: 7.1
  * Requires at least: 6.7
  * Requires PHP: 7.4
  * Description: This plugin adds the table of contents block.
